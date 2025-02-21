073eccf0080b60aa486120e3334e873627936c23
braynguyen <btn16@pitt.edu>
2025-02-21 12:41:50 -0500

    added companies and interviews

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from database import get_db_connection, create_table
+from database import get_db_connection, initialize_tables
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
@@ -6,13 +6,73 @@ import sqlite3
 
 # creating global reqs
 app = FastAPI()
-create_table()
+initialize_tables()
+
+
+@app.get("/restart")
+def clean_up():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    tables = cursor.execute("""
+                SELECT name FROM sqlite_master WHERE type='table';
+    """).fetchall()
+
+    for table in tables:
+        cursor.execute(f"DROP TABLE IF EXISTS {table[0]}")
+    
+    conn.commit()
+    conn.close()
+
+    initialize_tables()
+
+# class Company(BaseModel):
+#     name: str
+
+@app.get("/companies/{company_name}")
+def get_company(company_name: str):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM companies WHERE name = ?",
+        (company_name,)
+    )
+    items = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return items
+
+@app.get("/companies")
+def get_companies():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM companies")
+    items = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return items
+
+@app.post("/companies")
+def create_company(company: str):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # values should sanitize input
+    cursor.execute(
+        "INSERT INTO companies (name) VALUES (?)", 
+        (company,)
+    )
+    conn.commit()
+    conn.close()
+
+    return {
+        "name": company
+    }
 
 class Member(BaseModel):
     pitt_id: str
     fname: str
     lname: str
-    grad_year: int
+    grad_date: str
+    location: str
+    company: str
 
 @app.get("/members")
 def get_members():
@@ -28,31 +88,24 @@ def create_member(member: Member):
     conn = get_db_connection()
     cursor = conn.cursor()
 
+    # values should sanitize input
     cursor.execute(
-        "INSERT INTO members (pittId, fname, lname, gradYear) VALUES (?, ?, ?, ?)", 
-        (member.pitt_id, member.fname, member.lname, member.grad_year)
+        "INSERT INTO members (pittId, fname, lname, gradDate, location, company) VALUES (?, ?, ?, ?, ?, ?)", 
+        (member.pitt_id, member.fname, member.lname, member.grad_date, member.location, member.company)
     )
     conn.commit()
     conn.close()
+
+
+    # create company if it does not exist in the database
+    if not get_company(member.company):
+        create_company(member.company)
 
     return {
         "pittId": member.pitt_id,
         "fname": member.fname,
         "lname": member.lname,
-        "gradYear": member.grad_year
+        "gradDate": member.grad_date,
+        "location": member.location,
+        "company": member.company
     }
-
-# if __name__ == "__main__":
-#     if not check_tables(curr):
-#         create_tables(conn, curr)
-    
-#     insert_dummy_data(conn, curr, "btn16", "Brayden", "Nguyen", 2025)
-
-#     print(
-#         curr.execute("""
-#             SELECT * FROM members;
-#         """).fetchall()
-#     )
-    
-#     clear_tables(conn, curr)
-#     conn.close()
--- backend/database.py
+++ backend/database.py
@@ -7,49 +7,41 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-def create_table():
+def initialize_tables():
     conn = get_db_connection()
     cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS companies (
+            name TEXT PRIMARY KEY
+        )
+    """)
+
+    # graduated attribute can be inferred from grad year
+    # dates are stored in 'YYYY-MM-DD' format
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS members (
             pittId TEXT PRIMARY KEY,
             fname TEXT NOT NULL,
             lname TEXT NOT NULL,
-            gradYear INTEGER
+            gradDate INTEGER NOT NULL,
+            location TEXT,
+            company TEXT,
+            FOREIGN KEY (company) references companies(name)
+        )
+    """)
+
+    # dates are stored in 'YYYY-MM-DD' format
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS interviews (
+            company TEXT NOT NULL,
+            pittId TEXT NOT NULL,
+            date INTEGER NOT NULL,
+            role TEXT NOT NULL,
+            PRIMARY KEY (company, pittId, date),
+            FOREIGN KEY (pittId) REFERENCES members (pittId),
+            FOREIGN KEY (company) REFERENCES companies (name)
         )
     """)
     conn.commit()
     conn.close()
-
-create_table()
-
-def check_tables(conn: sqlite3.Connection, curr: sqlite3.Cursor) -> bool:
-    return bool(curr.execute("""
-        SELECT name FROM sqlite_master WHERE type='table' AND name='members';
-    """).fetchall())
-
-def create_tables(conn: sqlite3.Connection, curr: sqlite3.Cursor):
-    curr.execute("CREATE TABLE members(pitt_id, fname, lname, grad_date)")
-    print("Table created.")
-    conn.commit()
-
-
-def insert_dummy_data(conn, curr, pitt_id, fname, lname, year):
-    statement = """
-        INSERT INTO members
-                 (pitt_id, fname, lname, grad_date)
-                 values (?, ?, ?, ?);
-    """
-    curr.execute(statement, (pitt_id, fname, lname, year))
-    conn.commit()
-
-
-def clear_tables(conn, curr, ):
-    tables = curr.execute("""
-                SELECT name FROM sqlite_master WHERE type='table';
-    """).fetchall()
-
-    for table in tables:
-        curr.execute(f"DROP TABLE IF EXISTS {table[0]}")
-    
-    conn.commit()
